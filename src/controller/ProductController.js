7e67f3bfb3358a9560d22bae2ffd0b2bf8ce6d62
vikramsingh7568 <110588921+vikramsingh7568@users.noreply.github.com>
2022-10-12 16:33:35 +0530

    getbyid

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -10,7 +10,18 @@ const getByFilter = async function (req,res){
 }
 
 const getById = async function (req,res){
-    
+    let productId = req.params.productId
+    if(isValidId(productId)){
+       return  res.status(400).send({status : false , message : "please enter Valid productId ! "})
+    }
+       
+     let product = await productModel.findOne({_id : productId , isDeleted : false})
+     if(!product){
+       return  res.status(400).send({status : false , message : "this product is deleted "})
+     }
+
+      return res.status(200).send({status : true , data : product})
+
 }
 
 const updateProduct = async function (req,res){
