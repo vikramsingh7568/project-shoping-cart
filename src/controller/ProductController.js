66541528148696738dd05c1b9a0fea769c18ec65
shreya <10shreyad@gmail.com>
2022-10-12 15:49:23 +0530

    product controller updated

---
+++ src/controller/ProductController.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+
+const createProduct = async function (req,res){
+
+}
+
+const getByFilter = async function (req,res){
+    
+}
+
+const getById = async function (req,res){
+    
+}
+
+const updateProduct = async function (req,res){
+    
+}
+
+module.exports = {createProduct, getByFilter, getById, updateProduct}
