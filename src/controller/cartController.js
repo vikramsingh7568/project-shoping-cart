fd530e14597da9b3f81111896c23d48ce0d116ba
shreya <10shreyad@gmail.com>
2022-10-17 11:48:32 +0530

    cart model created

---
+++ src/controller/cartController.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+
+const createCart = async function(req,res){
+
+}
+
+const updateCart = async function(req,res){
+    
+}
+
+const getCartDetails = async function(req,res){
+    
+}
+
+const deleteCart = async function(req,res){
+    
+}
+
+module.exports = {createCart, updateCart, getCartDetails, deleteCart}
---
+++ src/models/cartModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: { type: ObjectId, ref: "User", required: true },
+    items: [
+      {
+        productId: { type: ObjectId, ref: "product", required: true },
+        quantity: { type: Number, required: true, default: 1 },
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+    totalItems: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("cart", cartSchema);
